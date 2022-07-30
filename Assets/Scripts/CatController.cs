78a665a23178c090d1a74fb144e1af9444592ffa
Michael Bayne <mdb@samskivert.com>
2022-07-29 20:44:39 -0700

    Show a crown on the leader.

--- Assets/Scripts/CatController.cs
+++ Assets/Scripts/CatController.cs
@@ -1,12 +1,15 @@
 namespace catwars {
 
+using UnityEngine;
 using UnityEngine.UI;
 
 public class CatController : DropTarget {
   private CatState cat;
 
+  public Icons icons;
   public FaceController face;
   public Image[] hungers;
+  public Image type;
 
   public void Init (CatState cat) {
     this.cat = cat;
@@ -19,12 +22,22 @@ public class CatController : DropTarget {
     });
 
     cat.acted.OnValue(acted => face.SetActive(!acted));
+
+    switch (cat.role.current) {
+    case CatState.Role.Leader: ShowTypeIcon(icons.crown); break;
+    // TODO: medicine cat icon
+    }
   }
 
   public override bool CanDrop (Phase phase, object dragState) =>
     phase == Phase.Eat && dragState is Prey;
 
   protected override object DropState => cat;
+
+  private void ShowTypeIcon (Sprite icon) {
+    type.sprite = icon;
+    type.gameObject.SetActive(true);
+  }
 }
 
 }
--- Assets/Scripts/Icons.cs
+++ Assets/Scripts/Icons.cs
@@ -6,12 +6,13 @@ using UnityEngine;
 public class Icons : ScriptableObject {
 
   [EnumArray(typeof(Prey))] public Sprite[] preyIcons;
+  public Sprite Prey (Prey prey) => preyIcons[(int)prey];
+
   [EnumArray(typeof(Herb))] public Sprite[] herbIcons;
+  public Sprite Herb (Herb herb) => herbIcons[(int)herb];
 
   public Sprite[] faces;
-
-  public Sprite Prey (Prey prey) => preyIcons[(int)prey];
-  public Sprite Herb (Herb herb) => herbIcons[(int)herb];
+  public Sprite crown;
 
   public override string ToString () => name;
 }
